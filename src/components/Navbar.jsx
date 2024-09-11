fdb4823b29ebac2aed72f672ada9ce229619acfa
Tommy Ashfield <tomash@udel.edu>
2024-09-11 16:28:49 -0400

    fix: logo reference

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
               window.scrollTo(0,0);
             }}
           >
-            <img src="src\assets\myLogo.png" alt="logo" className='w-9 h-9 object-contain'/>
+            <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
             <p className='text-white text-[18px] font-bold cursor-pointer'>Thomas Ashfield</p>
           </Link>
           <ul className='list-none hidden sm:flex flex-row gap-10'>
